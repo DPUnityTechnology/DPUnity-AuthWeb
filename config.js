ee980f61c46d9672ee925d322fe7612966a076c7
hongocquocsang2721@gmail.com <hongocquocsang2721@gmail.com>
2026-06-13 17:27:46 +0700

    Configure Google OAuth client

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.DPUNITY_SHEETS_CONFIG = {
     apiUrl: "https://script.google.com/macros/s/AKfycbyzIJByowKYdcu3t9V5Lbz0QrlNV6K6iLU08bcZLdNl2Kf6QYRmuSbxw3ZUinCQ6W0f/exec",
-    googleClientId: "YOUR_GOOGLE_OAUTH_CLIENT_ID"
+    googleClientId: "1051090965322-htemem6elmovjfpfj081cugqd684hguh.apps.googleusercontent.com"
 };
